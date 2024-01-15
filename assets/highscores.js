9adb5af4f225f000bcfb1309b4c1ee5f950ae9f1
Jetnik Syla <sjetnik@gmail.com>
2024-01-15 15:14:12 -0500

    Remove dot before initials in listItem.innerHTML

--- assets/highscores.js
+++ assets/highscores.js
@@ -61,7 +61,7 @@ function displayHighscores() {
   highscoresList.innerHTML = "";
   highscores.forEach((score) => {
     const listItem = document.createElement("li");
-    listItem.innerHTML = `.${score.initials}&nbsp;&nbsp;&nbsp;&nbsp;- ${score.score}`;
+    listItem.innerHTML = `${score.initials}&nbsp;&nbsp;&nbsp;&nbsp;- ${score.score}`;
 
     // Apply a random color to each highscore entry
     listItem.style.color = getRandomColor();
